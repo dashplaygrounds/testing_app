21e0ccdad7e1c02f874809b45c37c7e7dab6155b
Mark Silverio <dashplaygrounds@gmail.com>
2023-10-05 15:16:09 +0800

    feat: working scaffold

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ void main() {
         body: const Center(
           child: Image(
             image: NetworkImage(
-                'https://images.unsplash.com/photo-1483347756197-71ef80e95f73?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8bm9ydGhlcm4lMjBsaWdodHN8ZW58MHx8MHx8fDA%3D&w=1000&q=80'),
+                'http://minio2023.dashmark.me:9998/bucket1/photo-1483347756197-71ef80e95f73.jpg?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=myMinioAdmin%2F20231005%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20231005T071013Z&X-Amz-Expires=604800&X-Amz-SignedHeaders=host&X-Amz-Signature=d2be43c6c91baf637e974968c71ea4e7880a0eab71414eb7bb9338365547a589'),
           ),
         ),
       ),
